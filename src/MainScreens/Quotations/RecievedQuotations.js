dace31e805574e7eccdabbe40260a478312afe5b
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2020-09-10 15:53:59 +0100

    git NOw

--- src/MainScreens/Quotations/RecievedQuotations.js
+++ src/MainScreens/Quotations/RecievedQuotations.js
@@ -1,90 +1,163 @@
 import React from "react";
-import styles from 'styled-components'
+import styles from "styled-components";
+
 
 const Data = [
   {
     id: 0,
-    customerName:'Mr. Philip',
-    customerID: '#AT123456789',
-    customerEmail: 'example@exampleemail.com',
-    Msg: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-    className: 'isRead',
+    customerName: "User",
+    customerID: "user is not registered",
+    customerEmail: "example@exampleemail.com",
+    Msg:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    className: "isRead",
   },
   {
     id: 1,
-    customerName:'Mr. Philip',
-    customerID: '#AT123456789',
-    customerEmail: 'example@exampleemail.com',
-    Msg: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-    className: 'isReplied',
-  } ,
+    customerName: "Mr. Philip",
+    customerID: "#AT123456789",
+    customerEmail: "example@exampleemail.com",
+    Msg:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    className: "isReplied",
+  },
   {
     id: 2,
-    customerName:'Mr. Philip',
-    customerID: '#AT123456789',
-    customerEmail: 'example@exampleemail.com',
-    Msg: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-    className: 'isRead',
+    customerName: "Mr. John",
+    customerID: "#AT123456789",
+    customerEmail: "example@exampleemail.com",
+    Msg:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    className: "isRead",
   },
   {
     id: 3,
-    customerName:'Mr. Philip',
-    customerID: '#AT123456789',
-    customerEmail: 'example@exampleemail.com',
-    Msg: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-    className: 'isRead',
+    customerName: "Frank",
+    customerID: "user is not registered",
+    customerEmail: "example@exampleemail.com",
+    Msg:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    className: "isNotRead",
   },
   {
     id: 4,
-    customerName:'Mr. Philip',
-    customerID: '#AT123456789',
-    customerEmail: 'example@exampleemail.com',
-    Msg: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-    className: 'isReplied',
+    customerName: "Mr. Philip",
+    customerID: "#AT123456789",
+    customerEmail: "example@exampleemail.com",
+    Msg:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    className: "isReplied",
   },
   {
     id: 5,
-    customerName:'Mr. Philip',
-    customerID: '#AT123456789',
-    customerEmail: 'example@exampleemail.com',
-    Msg: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
-    className: 'isRead',
-  }
-]
-
-
-
+    customerName: "Mr. Philip",
+    customerID: "#AT123456789",
+    customerEmail: "example@exampleemail.com",
+    Msg:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    className: "isRead",
+  },
+];
 
 export default function RecievedQuotes() {
   return (
     <>
-     <div className="d-header">
-      <div className="d-header-menu">
-        <li className="d-header-menu-li">Recieved Quotations</li>
+      <div className="d-header">
+        <div className="d-header-menu">
+          <li className="d-header-menu-li">Recieved Quotations</li>
+        </div>
       </div>
-    </div> 
-<Container>
-    {Data.map((data) => (
-      <div className='quotation'>
-Hello
-      </div>
-    ))}
-    </Container>
-
-</>
+      <Container>
+        
+        {Data.map((data) => (
+          <div className={data.className}>
+            <div className="left">
+              <small>Request from</small>
+              <p>{data.customerName}</p>
+              <small>{data.customerID}</small>
+            </div>
+            <div className="middle">
+              <p>{data.customerEmail}</p>
+              <span>{data.Msg}</span>
+            </div>
+            <div className="right">
+              <button className="btn">View Message</button>
+            </div>
+          </div>
+        ))}
+      </Container>
+    </>
   );
 }
-
 
 const Container = styles.div`
 width: 100%;
 margin: auto;
 display: table;
-.quotation{
-  width: 80%;
-  margin: auto;
-  padding: 10px;
-  border: solid 2px;
-  border-left-color: yellow;
+.btn{
+  width: 70%;
+  margin: 30px;
+  background-color: #4CAF50;
+  color: #fff;
 }
-`
+.left{
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  p{
+    font-size: 20px;
+    font-weight: 600;
+    margin: 5px 0;
+  }
+}
+.middle{
+  width: 60%;
+  height: 80px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  p{
+    font-size: 17px;
+    font-weight: 500;
+    margin: 5px;
+  }
+  
+  span{
+    height: 40px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+.right{
+  width: 20%;
+}
+
+
+  .isRead{
+    width: 80%;
+    display: flex;
+    margin: 10px auto;
+    padding: 10px;
+    border-radius: 7px;
+    border: solid 1px #cac8c8;
+    border-left: solid 7px #2196F3;
+  }
+  .isReplied{
+    width: 80%;
+    display: flex;
+    margin: 10px auto;
+    padding: 10px;
+    border-radius: 7px;
+    border: solid 1px #cac8c8;
+    border-left: solid 7px #FF9800;
+  }
+  .isNotRead{
+    width: 80%;
+    display: flex;
+    margin: 10px auto;
+    padding: 10px;
+    border: solid 1px #cac8c8;
+    border-radius: 7px;
+    border-left: solid 7px #FF5722;
+  }
+`;
